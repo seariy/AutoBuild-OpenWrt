74f14591a3f4f16ce4896aa037c921f448555c6e
seariy <hailangzhisheng@gmail.com>
2024-11-23 23:30:12 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,7 +7,6 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.10.100/g' openwrt/package/base-files/files/bin/config_generate
-#sed -i 's/192.168.1.1/192.168.10.100/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
