f10c02bc25a960caca0706c6b576fbbfd59dd7a4
seariy <hailangzhisheng@gmail.com>
2024-11-24 19:53:55 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -15,7 +15,16 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-se
 sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.8/g' ./target/linux/x86/Makefile
 
 #4. 版本号里显示一个自己的名字
-sed -i "s/OpenWrt /seariy build $(TZ=UTC-8 date "+%Y.%m.%d") @ Seariy /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/OpenWrt /seariy build $(TZ=UTC-8 date "+%Y.%m.%d") @ Seariy /g" openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by Seariy '/g" openwrt/package/default-settings/files/zzz-default-settings
 
-#5. 修改主机名
-#sed -i '/uci commit system/i\uci set system.@system[0].hostname='OpenWrt'' openwrt/package/lean/default-settings/files/zzz-default-settings
+# 修改默认主机名
+sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-Seariy'/g" openwrt/package/base-files/files/bin/config_generate
+
+# iStore插件
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+./scripts/feeds update istore
+./scripts/feeds install -d y -p istore luci-app-store
+
+# 添加订阅源
+#sed -i '$a src-git istore https://github.com/linkease/istore.git;main' feeds.conf.default
