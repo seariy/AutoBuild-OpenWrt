3aa783364ed9278f09ba06772602fe809d50f00f
Seariy <hailangzhisheng@gmail.com>
2024-11-26 01:08:31 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,8 +5,6 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
-#sed -i 's/192.168.1.1/192.168.10.100/g' openwrt/package/base-files/files/bin/config_generate
 
 ########
 NET="openwrt/package/base-files/luci2/bin/config_generate"
@@ -33,17 +31,8 @@ sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
 
 ########
 
-#2. Clear the login password
+# Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
-
-#3. 指定内核（解决部分插件不兼容的问题）
-#sed -i 's/KERNEL_PATCHVER:=.*/KERNEL_PATCHVER:=6.8/g' ./target/linux/x86/Makefile
-
-#4. 版本号里显示一个自己的名字
-#sed -i "s/OpenWrt /seariy build $(TZ=UTC-8 date "+%Y.%m.%d") @ Seariy /g" openwrt/package/lean/default-settings/files/zzz-default-settings
-
-# 修改默认主机名
-#sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-Seariy'/g" openwrt/package/base-files/files/bin/config_generate
 
 # 添加订阅源
 #sed -i '$a src-git istore https://github.com/linkease/istore.git;main' feeds.conf.default
